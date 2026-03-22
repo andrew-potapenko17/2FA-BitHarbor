dfe740b02bbbcdda99295bac2e8d29b505f56b3d
Andrew (Win) <andrew.potapenko17@gmail.com>
2026-03-22 16:42:20 +0200

    fix #2

--- bitharbor/bitharbor/settings.py
+++ bitharbor/bitharbor/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'main.apps.MainConfig',
 ]
 
@@ -116,9 +115,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 LOGGING = {
     'version': 1,
